6bde2c5ef89d51064262a3684ad56666f6aa762b
Abhishta Gatya Adyatma <abhishtagatya@yahoo.com>
2019-03-08 16:03:42 +0700

    Code Review
    Some minor changes to code for better understanding and logic

--- firstPythonProgram.py
+++ firstPythonProgram.py
@@ -32,17 +32,28 @@ list.append(Account("chawza", "asdf", "Nabeel Kahlil Maulana", 18, "derp"))
 userInput = 0
 while userInput != 4:
     print("1. Log in\n2. Sign up\n3. show all signed up accounts\n4. Exits program")
-    userInput = int(input("User Choice: "))
+    
+    # Use Try - Except statements to catch potential errors, decrease user error
+    # Ref : https://realpython.com/python-exceptions/
+    try :
+        userInput = int(input("User Choice: "))
+    except ValueError:
+        print("Enter a number as input!")
+        continue
+        
     if userInput == 1:
         printBlanks()
         #user log in
         loginName = input("Enter your account username: ")
         loginPass = input("Enter your account password: ")
-        for a in range(len(list)):
-            if list[a].username == loginName:
-                if list[a].password == loginPass:
+        
+        # Iteration over list (you can iterate over list items without their index) promotes better practice
+        # Ref : https://thispointer.com/python-how-to-iterate-over-a-list/
+        for user in list:
+            if user.username == loginName:
+                if user.password == loginPass:
                     print("\nsecret massage is: ", end='')
-                    list[a].printMassage()
+                    user.printMassage()
                 else:
                     print("You write the wrong username or password, please try again")
 
@@ -56,6 +67,13 @@ while userInput != 4:
         newName = input("enter your Full name: ")
         newAge = input("enter your age: ")
         newMassage = input("enter your secret massage: ")
+        
+        # Add layers of verification that the input is valid
+        # i.e : newUsername = input() -> make sure there is no duplicate
+        #       newAge = input() -> returns a string, but wants an int
+        #       newPass = input() -> make sure is not emptty
+        #       newMassage = input() -> make sure is not empty
+        
         list.append(Account(newUsername, newPass, newName, newAge, newMassage))
         print("Welcome ",newUsername,", your account has been added!")
         #I should have put the input() in the constructor but fck it
@@ -64,8 +82,11 @@ while userInput != 4:
         #show all signed up accounts
         printBlanks()
         print("All availabe account registered:")
-        for a in range(len(list)):
-            print(list[a].username)
+        
+        # Check if list is not empty first, then iterate
+        
+        for user in list:
+            print(user.username)
         input("Press enter to continue...")
 
     elif userInput == 4:
