13effa9bd8b1f10af5bba024b7b95460b082eb53
NabeelKM <nabeelkahlil403@gmail.com>
2020-02-12 16:58:16 +0700

    Create K-means.py

---
+++ ML Algorithm/K-means.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import random
+
+class data:
+    def __init__(self, x, y):
+        self.xAxis = x
+        self.yAxis = y
+    cluster = 0
+
+def rect_distance(a, b):
+    return abs(a.xAxis - b.xAxis) + abs(a.yAxis - b.yAxis)
+
+# initialize
+arrayList = []
+dataLength = 50
+centroid = []
+centroid_number =  3
+
+#intial the sentroid location
+for strd in range(centroid_number):
+    x = random.randint(1, 100)
+    y = random.randint(1, 100)
+    centroid.append(data(x,y))
+
