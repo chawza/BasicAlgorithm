ae29404aa4693564704f6fb481332cc3f896443e
Nabeel Kahlil Maulana <nabeelkahlil403@gmail.com>
2020-02-28 22:25:34 +0700

    First upload
    A simple code to calculate the activation functions for ANN

---
+++ activation_functions.py
@@ -0,0 +1,41 @@
+import math
+import numpy as np
+
+# variables
+np.random.seed(0)
+weight = np.array(np.random.rand(5))   #prerandom number
+input = np.array([2, -1, 4, -3, 1])
+
+#declare functions
+def sigmoid(a):
+    return 1/(1 + np.exp(-(weight.dot(input.T))))
+
+def step(a):
+    if a >= 0:
+        return 1
+    else:
+        return 0
+
+def tanh(a):
+    return (np.exp(a) - np.exp(-a)) / (np.exp(a) + np.exp(-a))
+
+def ReLu(a):
+    if a <= 0:
+        return 0
+    return a
+
+# reference: https://mlfromscratch.com/activation-functions-explained/#/
+def gaussian(a):
+    number = np.sqrt(2 / (np.pi * (a + 0.044715 * np.power(a, 3))))
+    return 0.5 * a * (1 + tanh(number))
+
+num = weight.dot(input.T)
+
+print("weight\t: ", weight)
+print('input\t: ', input)
+print('\nLinear\t: ', num)
+print('step\t: ', step(num))
+print('sigmoid\t: ', sigmoid(num))
+print('tanH\t: ', tanh(num))
+print('ReLu\t: ', ReLu(num))
+print('Gaussian: ', gaussian(num))
